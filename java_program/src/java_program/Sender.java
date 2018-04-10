f23d8fa96c2c40de4773396852bce9ab487f74b8
baejaeeon <baejaeeon@175.117.160.221>
2018-04-11 00:24:13 +0900

    로그 제거

--- java_program/src/java_program/Sender.java
+++ java_program/src/java_program/Sender.java
@@ -79,8 +79,6 @@ public class Sender {
 							// 클라이언트로 부터 ID 값 받아오기
 							int random_number_id = ((RandomNumber) receiver.readObject()).getRandom_number(); // 수신
 							
-							System.out.println(random_number_id);
-							
 							RandomNumber randomNumber = new RandomNumber();
 							randomNumber.setRandom_number_id(random_number_id);
 							
